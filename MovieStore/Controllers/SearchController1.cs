7d9962a581b5dd47f222354ba23ee11f589026fb
beygel <hbeygel@gmail.com>
2016-06-14 20:40:33 +0300

    search works with movie genres filtering

---
+++ MovieStore/Controllers/SearchController1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MovieStore.Models;
+
+namespace MovieStore.Controllers
+{
+    public class SearchController1 : Controller
+    {
+        private MovieStoreEntities db = new MovieStoreEntities();
+
+        // GET: SearchController1
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Movies.ToListAsync());
+        }
+
+        // GET: SearchController1/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = await db.Movies.FindAsync(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        // GET: SearchController1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SearchController1/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "MovieId,Title,Length,Price,Amount,Date")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Movies.Add(movie);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(movie);
+        }
+
+        // GET: SearchController1/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = await db.Movies.FindAsync(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: SearchController1/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "MovieId,Title,Length,Price,Amount,Date")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(movie).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(movie);
+        }
+
+        // GET: SearchController1/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = await db.Movies.FindAsync(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: SearchController1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Movie movie = await db.Movies.FindAsync(id);
+            db.Movies.Remove(movie);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MovieStore/Utilities/Searching/SearchManager.cs
@@ -0,0 +1,20 @@
+﻿using MovieStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MovieStore.Utilities.Searching
+{
+    class SearchManager
+    {
+        MovieStoreEntities db = new MovieStoreEntities();
+
+        public async Task<IQueryable<Movie>> GetFilteredMoviesAsync(string movieGenre, string searchString)
+        {
+            return db.Movies.Where(m => m.MovieGenres.ToList().Find(mg => mg.Genre.TItle.Contains(movieGenre)) != null &&
+                                                                           m.Title.Contains(searchString));
+        }
+    }
+}
