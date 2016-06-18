04d835ef4811e88e09f8c74bdd00cc629f239f5d
idan dagan <daganida@post.bgu.ac.il>
2016-06-18 21:44:36 +0300

    updated all necessary things

--- MovieStore/Controllers/SearchController.cs
+++ MovieStore/Controllers/SearchController.cs
@@ -19,7 +19,10 @@ namespace MovieStore.Controllers
       
 
         public ActionResult Index(string sortOrder, string currentFilter, string movieGenre, string searchString, int? page)
+        
         {
+            List<Movie> recommendedMovies = getRecommendedMovies();
+
 
             var GenreQry = from genre in db.Genres
                            select genre.TItle;
@@ -71,8 +74,9 @@ namespace MovieStore.Controllers
 
         int pageSize = 5;
             int pageNumber = (page ?? 1);
-            IPagedList list = movies.ToPagedList(pageNumber, pageSize);
-            return View(list);
+            IPagedList<Movie> list = movies.ToPagedList(pageNumber, pageSize);
+            return View(new Tuple<IPagedList<Movie>, IEnumerable<Movie>>(list, recommendedMovies));
+
         }
         public ActionResult openMovieDetails(int? id)
         {
@@ -99,5 +103,198 @@ namespace MovieStore.Controllers
             CartsController.addNewCartToDB(Int32.Parse(userId), id.Value);
             return RedirectToAction("Index");
         }
+        private List<Movie> getRecommendedMovies()
+        {
+            List<Movie> solutionList = new List<Movie>();
+            //Dictionary<userId,amount> of match movies//
+            Dictionary<int, int> ratedMoviesForUser = new Dictionary<int, int>();
+            Dictionary<string, int> finalPrefferedMovie = new Dictionary<string, int>();
+
+            string userLoggedIn = CookieController.GetCookie("userId");
+            if (userLoggedIn != null)
+            {
+                var userOrders = db.Orders.SqlQuery("Select * from dbo.Orders where dbo.Orders.UserId = {0}", Convert.ToInt32(userLoggedIn));
+                foreach (Order o in userOrders)
+                {
+                    //foreach movie order we want to find the clients the bought the movie
+                    //if theres a match we add to dictionary
+                    var MovieOrdersForUser = db.MovieOrders.SqlQuery("Select * from dbo.MovieOrders where dbo.MovieOrders.OrderId = {0}", o.OrderId);
+
+                    //now we add to dictionary if there is a match
+                    foreach (MovieOrder mo in MovieOrdersForUser)
+                    {
+                        //now we get all users for each movieId
+                        var userOrdersForSpecificMovie = db.MovieOrders.SqlQuery("select * from dbo.MovieOrders where dbo.MovieOrders.MovieId = {0}", mo.MovieId);
+
+                        foreach (MovieOrder usersMatchForMovie in userOrdersForSpecificMovie)
+                        {
+                            var order = db.Orders.SqlQuery("select * from dbo.Orders where dbo.Orders.OrderId = {0}", usersMatchForMovie.OrderId).ToList()[0];
+                            int userId = order.UserId;
+                            if (order.UserId != o.UserId)
+                            {
+                                if (!ratedMoviesForUser.ContainsKey(userId))
+                                {
+                                    ratedMoviesForUser.Add(userId, 1);
+                                }
+                                else
+                                {
+                                    ratedMoviesForUser[userId]++;
+                                }
+                            }
+
+
+                        }
+
+                    }
+
+
+                }
+                //now we want to sort the dictionary by matches
+                List<KeyValuePair<int, int>> myList = ratedMoviesForUser.ToList();
+
+                myList.Sort(
+                    delegate(KeyValuePair<int, int> pair1,
+                    KeyValuePair<int, int> pair2)
+                    {
+                        return pair2.Value.CompareTo(pair1.Value);
+                    }
+                );
+                //now we have sort list, need to return the top 5.
+                foreach (KeyValuePair<int, int> user in myList)
+                {
+                    //we get all order of the person
+                    //and foreach order we go on all the movies
+                    var allUserOrders = db.Orders.SqlQuery("select * from dbo.orders where UserId = {0}", user.Key);
+
+                    foreach (Order order in allUserOrders)
+                    {
+                        //get all the movie orders for a specific order
+                        var movieOrdersForUser = db.MovieOrders.SqlQuery("select * from dbo.MovieOrders where dbo.MovieOrders.OrderId = {0}", order.OrderId);
+                        foreach (MovieOrder mo in movieOrdersForUser)
+                        {
+                            if (Data.getInstance().getDictionary().ContainsKey(mo.MovieId))
+                            {
+                                string movieName = Data.getInstance().getDictionary()[mo.MovieId];
+                                if (finalPrefferedMovie.ContainsKey(movieName))
+                                {
+                                    finalPrefferedMovie[movieName]++;
+                                }
+                                else
+                                {
+                                    finalPrefferedMovie.Add(movieName, 1);
+                                }
+                            }
+
+                        }
+
+                    }
+
+                }
+                //now we have all the movies and all the matches from all users
+                //we need to sort by value
+                List<KeyValuePair<string, int>> myFinalList = finalPrefferedMovie.ToList();
+                //the list that we suppose to return
+                solutionList = new List<Movie>();
+
+                myFinalList.Sort(
+                    delegate(KeyValuePair<string, int> pair1,
+                    KeyValuePair<string, int> pair2)
+                    {
+                        return pair2.Value.CompareTo(pair1.Value);
+                    }
+                );
+                //we need the best 5
+                int counter = 1;
+                foreach (KeyValuePair<string, int> tuple in myFinalList)
+                {
+                    if (counter == 5)
+                        break;
+                    Movie m = db.Movies.SqlQuery("select * from dbo.Movies where dbo.Movies.Title = {0}", tuple.Key).ToList()[0];
+                    solutionList.Add(m);
+                    counter++;
+
+                }
+                solutionList = FillListWithPopularMovies(solutionList);
+
+
+            }
+            else
+            {
+                solutionList = FillListWithPopularMovies(solutionList);
+            }
+            return solutionList;
+
+        }
+
+        private List<Movie> FillListWithPopularMovies(List<Movie> solutionList)
+        {
+            Dictionary<string, int> MovieBought = new Dictionary<string, int>();
+            var movieOrders = db.MovieOrders;
+            int counter = solutionList.Count;
+            if (counter < 5)
+            {
+                foreach (MovieOrder mo in movieOrders)
+                {
+
+                    int movieId = mo.MovieId;
+                    if (Data.getInstance().getDictionary().ContainsKey(movieId))
+                    {
+                        string movieName = Data.getInstance().getDictionary()[movieId];
+                        if (!MovieBought.ContainsKey(movieName))
+                        {
+                            MovieBought.Add(movieName, 1);
+                        }
+                        else
+                        {
+                            MovieBought[movieName]++;
+                        }
+                    }
+                }
+                //now osrt the dictionary
+                List<KeyValuePair<string, int>> myFinalList = MovieBought.ToList();
+                myFinalList.Sort(
+                       delegate(KeyValuePair<string, int> pair1,
+                       KeyValuePair<string, int> pair2)
+                       {
+                           return pair2.Value.CompareTo(pair1.Value);
+                       }
+                   );
+                foreach (KeyValuePair<string, int> tuple in myFinalList)
+                {
+                    if (counter == 5)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Movie m = db.Movies.SqlQuery("select * from dbo.Movies where dbo.Movies.Title = {0}", tuple.Key).ToList()[0];
+
+                        if (!solutionList.Contains(m))
+                        {
+                            solutionList.Add(m);
+                            counter++;
+                        }
+
+
+                    }
+
+                }
+                return solutionList;
+            }
+            else
+            {
+                return solutionList;
+            }
+
+
+
+
+
+
+
+
+        }
+
+
     }
 }
